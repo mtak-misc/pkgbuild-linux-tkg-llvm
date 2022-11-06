194f27b132b55fd3a9815f300cd83c3d5bbaa792
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2022-11-06 20:00:18 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ curl -sL https://$GITHUB_TOKEN@api.github.com/repos/mtak-misc/pkgbuild-llvm-git/
 | jq '.assets[] | select(.name | contains("llvm")) | .id')" -o llvm-git.zip
 unzip llvm-git.zip
 pacman --noconfirm -U llvm-git*.pkg.tar.zst llvm-libs-git*.pkg.tar.zst
-useradd builder -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+useradd builder  -u $USERID -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 chown builder -R /workspace
 chown builder -R /tmp
 cd /workspace/linux-tkg ; su builder -c "yes '' | makepkg --noconfirm -sc"
