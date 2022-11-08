4f18269f93d198a4b90ef3b555d142cb6a207207
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2022-11-08 18:04:01 +0900

    Update update-config.sh

--- update-config.sh
+++ update-config.sh
@@ -3,5 +3,5 @@ curl -LO https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/
 NEWCONFIG=$(/usr/bin/sha256sum config | /usr/bin/awk '{print $1}')
 OLDCONFIG=$(/usr/bin/sha256sum config.x86_64 | /usr/bin/awk '{print $1}')
 
-echo "-i 's/"$OLDCONFIG"/"$NEWCONFIG"/' ../../PKGBUILD" | /usr/bin/xargs /usr/bin/sed
+eval "/usr/bin/sed -i 's/"$OLDCONFIG"/"$NEWCONFIG"/' ../../PKGBUILD"
 mv config config.x86_64
